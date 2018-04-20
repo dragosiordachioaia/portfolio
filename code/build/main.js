2f617b6c3b2dd930536b6f72b6d87abaf78f9315
dragosiordachioaia <xdragosy@gmail.com>
2018-04-20 10:22:05 +0100

    made code more robust

--- code/build/main.js
+++ code/build/main.js
@@ -1,6 +1,22 @@
-TweenMax.to($('#first-half'), 0.7, {width: '50vw', delay: 0.5, ease: Power2.easeInOut});
-setTimeout(function() {
-  $('#nice').show();
-}, 2000);
-TweenMax.to($('#hi'), 0.3, {left: '40%', delay: 1.7, ease: Power2.easeOut});
-TweenMax.to($('#nice'), 0.3, {left: '52%', delay: 2.7, ease: Power2.easeOut});
+TweenMax.to($('#first-half'), 0.7, {width: '50vw', delay: 0.5, ease: Power2.easeInOut, onComplete: function() {
+
+
+  var halfWidth = $(window).width();
+  var hi = $('#hi');
+  var hiWidth = hi.width();
+  var hiStartRight = halfWidth/2 - hiWidth - 10;
+  var hiEndRight = hiStartRight + hiWidth + 20;
+
+  hi.css('right', hiStartRight + 'px');
+  TweenMax.to(hi, 0.3, {right: hiEndRight, delay: 0.7, ease: Power2.easeOut, onComplete: function() {
+    var nice = $('#nice');
+    var niceWidth = nice.width();
+    var niceStartLeft = halfWidth/2 - niceWidth - 10;
+    var niceEndLeft = niceStartLeft + niceWidth + 20;
+    nice.css({
+      left: niceStartLeft + 'px',
+      display: 'inline',
+    });
+    TweenMax.to(nice, 0.3, {left: niceEndLeft, delay: 1, ease: Power2.easeOut});
+  }});
+}});
