7db870e22495c40c10ac6ba682b84cf6b4a70572
dragosiordachioaia <xdragosy@gmail.com>
2018-04-20 12:13:49 +0100

    added animation to the dot in 'Hi.'

--- code/build/main.js
+++ code/build/main.js
@@ -3,12 +3,15 @@ TweenMax.to($('#first-half'), 0.7, {width: '50vw', delay: 0.5, ease: Power2.ease
 
   var halfWidth = $(window).width();
   var hi = $('#hi');
-  var hiWidth = hi.width();
+  var hiWidth = hi.width() + 20;
   var hiStartRight = halfWidth/2 - hiWidth - 10;
-  var hiEndRight = hiStartRight + hiWidth + 20;
+  var hiEndRight = hiStartRight + hiWidth + 40;
+
+  var hiDot = $('#hi-dot');
 
   hi.css('right', hiStartRight + 'px');
   TweenMax.to(hi, 0.3, {right: hiEndRight, delay: 0.7, ease: Power2.easeOut, onComplete: function() {
+    TweenMax.to(hiDot, 0.7, {scale: 1, delay: 0.3, ease: Elastic.easeOut})
     var nice = $('#nice');
     var niceWidth = nice.width();
     var niceStartLeft = halfWidth/2 - niceWidth - 10;
