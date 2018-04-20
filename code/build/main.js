da9b8b28e464a1749712351094471fcf4138761c
dragosiordachioaia <xdragosy@gmail.com>
2018-04-20 16:48:45 +0100

    finished frame 2

--- code/build/main.js
+++ code/build/main.js
@@ -1,25 +1,42 @@
-TweenMax.to($('#first-half'), 0.7, {width: '50vw', delay: 0.5, ease: Power2.easeInOut, onComplete: function() {
+frame1();
 
+function frame1() {
+  TweenMax.to($('#first-half'), 0.7, {width: '100vw', delay: 0.5, ease: Power2.easeInOut, onComplete: function() {
+    var halfWidth = $(window).width();
+    var hi = $('#hi');
+    var hiWidth = hi.width() + 20;
+    var hiStartRight = halfWidth/2 - hiWidth - 10;
+    var hiEndRight = hiStartRight + hiWidth + 40;
 
-  var halfWidth = $(window).width();
-  var hi = $('#hi');
-  var hiWidth = hi.width() + 20;
-  var hiStartRight = halfWidth/2 - hiWidth - 10;
-  var hiEndRight = hiStartRight + hiWidth + 40;
+    var hiDot = $('#hi-dot');
 
-  var hiDot = $('#hi-dot');
-
-  hi.css('right', hiStartRight + 'px');
-  TweenMax.to(hi, 0.3, {right: hiEndRight, delay: 0.7, ease: Power2.easeOut, onComplete: function() {
-    TweenMax.to(hiDot, 0.7, {scale: 1, delay: 0.3, ease: Elastic.easeOut})
-    var nice = $('#nice');
-    var niceWidth = nice.width();
-    var niceStartLeft = halfWidth/2 - niceWidth - 10;
-    var niceEndLeft = niceStartLeft + niceWidth + 20;
-    nice.css({
-      left: niceStartLeft + 'px',
-      display: 'inline',
-    });
-    TweenMax.to(nice, 0.3, {left: niceEndLeft, delay: 1, ease: Power2.easeOut});
+    hi.css('right', hiStartRight + 'px');
+    TweenMax.to(hi, 0.3, {right: hiEndRight, delay: 0.7, ease: Power2.easeOut, onComplete: function() {
+      TweenMax.to(hiDot, 0.7, {scale: 1, delay: 0.3, ease: Elastic.easeOut})
+      var nice = $('#nice');
+      var niceWidth = nice.width();
+      var niceStartLeft = halfWidth/2 - niceWidth - 10;
+      var niceEndLeft = niceStartLeft + niceWidth + 20;
+      nice.css({
+        left: niceStartLeft + 'px',
+        display: 'inline',
+      });
+      TweenMax.to(nice, 0.3, {left: niceEndLeft, delay: 1, ease: Power2.easeOut, onComplete: function() {
+        var iam = $('#iamdragos');
+        hi.css('z-index', 0);
+        TweenMax.to(hi, 0.2, {right: hiStartRight, delay: 1.5, onComplete: function() {
+          hi.hide();
+          TweenMax.to(nice, 0.2, {left: niceStartLeft, ease: Power2.easeOut, onComplete: function() {
+            nice.hide();
+            TweenMax.to($('#first-half'), 0.7, {right: '0', backgroundColor: '#31ceb7', delay: 0.5, ease: Power2.easeInOut, onComplete: function() {
+              var andthis = $('#andthis');
+              andthis.css('display', 'inline');
+              TweenMax.to(iam, 0.5, {left: '0', delay: 0.5, ease: Power2.easeInOut});
+              TweenMax.to($('#first-half'), 0.5, {left: '-50%', delay: 0.5, ease: Power2.easeInOut});
+            }});
+          }});
+        }});
+      }});
+    }});
   }});
-}});
+}
